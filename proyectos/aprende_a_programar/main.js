e6d6379a41dd1fb252e38392c95c5be1766ec486
Dionicio Hurtado <hurtado82@gmail.com>
2023-05-25 22:22:50 -0500

    responsive de portafolio y muestra1

---
+++ proyectos/aprende_a_programar/main.js
@@ -0,0 +1,12 @@
+const ulMenu = document.querySelector(".ul-menu")
+const btnMenu = document.querySelector("#btn-menu")
+const logo = document.querySelector("#logo")
+
+btnMenu.addEventListener("click", function(e){
+  ulMenu.classList.toggle("ul-menu-visible")
+  if(e.target.innerText === "menu") {
+    e.target.innerText="Close"
+  }else {
+    e.target.innerText="menu"
+  }
+})
