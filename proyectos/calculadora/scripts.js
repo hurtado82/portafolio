4596b0502dcb1fc9292423ae8317f24805b7bc20
Dionicio Hurtado <hurtado82@gmail.com>
2023-06-12 23:05:27 -0500

    refactorizacion del codigo en proceso

--- proyectos/calculadora/scripts.js
+++ proyectos/calculadora/scripts.js
@@ -1,11 +1,12 @@
-var valor 
-var v3
-var v1
-var mas = false
-var menos = false
-var multi = false
-var div = false
-var conteo = 0
+let v2 = 0
+let v1 = 0
+let valor = 0
+let mas = false
+let menos = false
+let multi = false
+let div = false
+let conteo = 0
+let show = document.querySelector("#show")
 
 const resetOperator = () => {
   mas = !true
@@ -13,211 +14,120 @@ const resetOperator = () => {
   multi = !true
   div = !true
 }
-const equalOpertor = (operator) => {
-  // valor = operator
-  console.log(v1, "v1 arriba",v3)
+
+const equalOperator = () => {
+
   if (mas) {
-    show.innerHTML = v3 + v1
-    console.log(v3+v1)
+    show.value = parseFloat(v1) + parseFloat(v2)
   }
   if (menos) {
-    show.innerHTML = v3 - v1
-    console.log(v3-v1)
+    show.value = parseFloat(v1) - parseFloat(v2)
   }
   if (multi) {
-    show.innerHTML = v3 * v1
-    console.log(v3*v1)
+    show.value = parseFloat(v1) * parseFloat(v2)
   }
   if (div) {
-    show.innerHTML = v3 / v1
-    console.log(v3/v1)
+    show.value = parseFloat(v1) / parseFloat(v2)
   }
+}
+
+const btnEvent = (e) => {
+  console.log(e)
+  valor+= e.target.value
+  show.value = valor.slice(1)
+  if(v1) {
+    v2 = show.value
+  }
+}
+
+const operatorsKeys = () => {
+  equalOperator()
+  v1 = show.value
+  valor = 0
+  resetOperator()
 }
 
 var boton1 = document.querySelector("#number1")
-.addEventListener("click", function(){
-valor = parseInt(this.value)
-console.log(valor)
-print(valor)
-})
-console.log(boton1)
+// boton1.addEventListener("click", (e) => btnEvent(e))
+boton1.addEventListener("onkeyup", (e) =>  console.log(e.target) )
+
 var boton2 = document.querySelector("#number2")
-.addEventListener("click", function(){
-valor = parseInt(this.value)
-  console.log(valor)
-  print(valor)
-})
+boton2.addEventListener("click", (e) => btnEvent(e))
 
 var boton3 = document.querySelector("#number3")
-.addEventListener("click", function(){
-  valor = parseInt(this.value)
-  console.log(valor)
-  print(valor)
-})
+boton3.addEventListener("click", (e) => btnEvent(e))
 
 var boton4 = document.querySelector("#number4")
-.addEventListener("click", function(){
-  valor = parseInt(this.value)
-  console.log(valor)
-  print(valor)
-})
+boton4.addEventListener("click", (e) => btnEvent(e))
 
 var boton5 = document.querySelector("#number5")
-.addEventListener("click", function(){
-  valor = parseInt(this.value)
-  console.log(valor)
-  print(valor)
-})
+boton5.addEventListener("click", (e) => btnEvent(e))
 
 var boton6 = document.querySelector("#number6")
-.addEventListener("click", function(){
-  valor = parseInt(this.value)
-  console.log(valor)
-  print(valor)
-})
+boton6.addEventListener("click", (e) => btnEvent(e))
 
 var boton7 = document.querySelector("#number7")
-.addEventListener("click", function(){
-  valor = parseInt(this.value)
-  valor1 = valor
-  console.log(valor)
-  print(valor)
-})
+boton7.addEventListener("click", (e) => btnEvent(e))
 
 var boton8 = document.querySelector("#number8")
-.addEventListener("click", function(){
-valor = parseInt(this.value)
-  console.log(valor)
-  print(valor)
-})
+boton8.addEventListener("click", (e) => btnEvent(e))
 
 var boton9 = document.querySelector("#number9")
-.addEventListener("click", function(){
-  valor = parseInt(this.value)
-  console.log(valor)
-  print(valor)
-})
+boton9.addEventListener("click", (e) => btnEvent(e))
 
 var boton0 = document.querySelector("#number0")
-.addEventListener("click", function(){
-  valor = parseInt(this.value)
-  console.log(valor)
-  print(valor)
-})
+boton0.addEventListener("click", (e) => btnEvent(e))
+
+var botonPunto = document.querySelector("#numberPunto")
+botonPunto.addEventListener("click", (e) => btnEvent(e))
 
 var botonMas = document.querySelector("#numberSuma")
-.addEventListener("click", function(){
-  console.log(v1)
-  v3 = parseInt(show.textContent)
-  valor = this.value
-  console.log(valor)
-  show.innerHTML = 0
-  console.log(v3 + " 'v3+'")
-  resetOperator()
-  mas = true
-  console.log(v1, "v1")
-  console.log(v3, "v3")
-  // if(v3 && v1) equalOpertor()
-  console.log(show)
+botonMas.addEventListener("click", function(){
+  if(show.value !== "0") {
+    operatorsKeys(mas)
+    mas = true
+  }
 })
 
 var botonResta = document.querySelector("#numberResta")
-.addEventListener("click", function(){
-  v3 = parseInt(show.textContent)
-  valor = this.value
-  console.log(valor)
-  show.textContent = 0
-  console.log(v3 + " 'v3-'")
-  resetOperator()
-  menos = true
-  // equalOpertor()
+botonResta.addEventListener("click", function(){
+  if(show.value !== "0") {
+    operatorsKeys(menos)
+    menos = true
+  }
 })
 
 var botonMulti = document.querySelector("#numberMulti")
-.addEventListener("click", function(){
-  v3 = parseInt(show.textContent)
-  valor = this.value
-  console.log(valor)
-  show.textContent = 0
-  console.log(v3 + " 'v3*'")
-  resetOperator()
-  multi = true
-  // equalOpertor()
+botonMulti.addEventListener("click", function(){
+  if(show.value !== "0") {
+    operatorsKeys(multi)
+    multi = true
+  }
 })
 
 var botonDiv = document.querySelector("#numberDiv")
-.addEventListener("click", function(){
-  v3 = parseInt(show.textContent)
-  valor = this.value
-  console.log(valor)
-  show.textContent = 0  
-  console.log(v3 + " 'v3/'")
-  resetOperator()
-  div = true
-  // equalOpertor()
-})
-
-
-// Mostrar en pantalla
-// document.querySelector("#show").textContent
-function print(v){
-  console.log(v3)
-
-  if(show.textContent === "0"){
-    show.innerHTML= v
-    v1 = parseInt(show.textContent)
-    console.log(v1, "v1", v3, "v3")
-  } else{
-    show.innerHTML+= v
-    conteo = show.textContent
-    console.log("conteo "+ conteo.length)
-    if (mas || menos || multi || div) {
-      v1 = parseInt(show.innerHTML)
-    }
-    console.log(v1)
+botonDiv.addEventListener("click", function(){
+  if(show.value !== "0") {
+    operatorsKeys(div)
+    div = true
   }
-}
-var botonEqual = document.querySelector("#numberIgual")
-.addEventListener("click", equalOpertor )
-  // valor = this.value
-  // console.log(valor)
-//   if (mas) {
-//     show.innerHTML = v3 + v1
-//     console.log(v3+v1)
-//   }
-//   if (menos) {
-//     show.innerHTML = v3 - v1
-//     console.log(v3-v1)
-//   }
-//   if (multi) {
-//     show.innerHTML = v3 * v1
-//     console.log(v3*v1)
-//   }
-//   if (div) {
-//     show.innerHTML = v3 / v1
-//     console.log(v3/v1)
-//   }
-// })
-var botonC = document.querySelector("#numberC")
-.addEventListener("click", function(dato){
-  show.innerHTML = 0
-  v1 = 0
-  console.log(v3)
-  console.log(v1)
-  valor = 0
-  console.log(valor)
-
-  mas = false
-  menos = false
-  multi = false
-  div = false
-
 })
+
+var botonEqual = document.querySelector("#numberIgual")
+botonEqual.addEventListener("click", equalOperator )
+  
 var botonCE = document.querySelector("#numberCE")
-.addEventListener("click", function(dato){
-  v3 = 0
+botonCE.addEventListener("click", function(){
+  show.value = 0
+  v2 = 0
+  valor = 0
+})
+
+var botonC = document.querySelector("#numberC")
+botonC.addEventListener("click", function(){
+  resetOperator()
+  v2 = 0
   v1 = 0
   valor = 0
-  show.innerHTML = 0
-  console.log(valor)
+  show.value = 0
 })
